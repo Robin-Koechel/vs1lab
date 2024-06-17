e18e6e6d49d81b3e2cacee769b877b4f9a2aff5b
koro1027 <koro1027@h-ka.de>
2024-06-17 14:00:24 +0000

    fix 4.1.1 & 4.1.2

--- Aufgabe4/gta_v4/app.js
+++ Aufgabe4/gta_v4/app.js
@@ -15,7 +15,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index copy');
+const indexRouter = require('./routes/index');
 
 /**
  * Set up Express app.
